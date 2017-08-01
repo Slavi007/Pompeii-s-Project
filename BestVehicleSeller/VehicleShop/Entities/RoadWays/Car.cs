2316ffa5bb17f87a7a9549ec606af23e01d5a14f
Radostin Levov <radostin.levov@gmail.com>
2017-08-02 01:39:56 +0300

    Added some properties in Car class.

--- BestVehicleSeller/VehicleShop/Entities/RoadWays/Car.cs
+++ BestVehicleSeller/VehicleShop/Entities/RoadWays/Car.cs
@@ -2,5 +2,18 @@
 {
     public abstract class Car : RoadWay
     {
+        public string Model { get; protected set; }
+               
+        public string Color { get; protected set; }
+
+        public int HorsePower { get; protected set; }
+
+        public double TopSpeed { get; protected set; }
+
+        public double Acceleration { get; protected set; }
+
+        public int NumberOfSeats { get; protected set; }
+
+        public int BootCapacity { get; protected set; }
     }
 }
