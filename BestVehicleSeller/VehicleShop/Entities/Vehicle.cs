9e3e113f39c9c10941a95e571aebfb613fc268d8
Slavi007 <lordi_slavi@abv.bg>
2017-08-04 00:40:16 +0300

    Added some properties to Vehicle and IVehicle, have to discuss which properties should be in the abstract class.

--- BestVehicleSeller/VehicleShop/Entities/Vehicle.cs
+++ BestVehicleSeller/VehicleShop/Entities/Vehicle.cs
@@ -1,11 +1,17 @@
-﻿using VehicleShop.Interfaces;
+﻿using System;
+using VehicleShop.Interfaces;
 
 namespace VehicleShop.Entities
 {
     public abstract class Vehicle : IVehicle
     {
-        public double Speed { get; }
+        public string CarMaker { get; }
+        public string Model { get; }
+        public double MaxSpeed { get; }
         public decimal Price { get; }
         public double LoadCapacity { get; }
+        public string Colour { get; }
+        public double Acceleration { get; }
+        public DateTime OriginDate { get; }
     }
 }
--- BestVehicleSeller/VehicleShop/Interfaces/IVehicle.cs
+++ BestVehicleSeller/VehicleShop/Interfaces/IVehicle.cs
@@ -1,11 +1,22 @@
-﻿namespace VehicleShop.Interfaces
+﻿using System;
+
+namespace VehicleShop.Interfaces
 {
     public interface IVehicle
     {
-        double Speed { get ; }
+        string CarMaker { get; }
+
+        string Model { get; }
+        double MaxSpeed { get ; }
 
         decimal Price { get ; }
 
         double LoadCapacity {get; }
+
+        string Colour { get; }
+
+        double Acceleration { get; }
+
+        DateTime OriginDate { get; }
     }
 }
