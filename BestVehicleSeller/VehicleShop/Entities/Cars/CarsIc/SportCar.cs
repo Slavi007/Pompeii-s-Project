78fb16c1f81fad7183675451a18d6cdeb8c3989c
Radostin Levov <radostin.levov@gmail.com>
2017-08-10 01:34:10 +0300

    Added new interface IBodyKit.

--- BestVehicleSeller/VehicleShop/Entities/Cars/CarsIc/SportCar.cs
+++ BestVehicleSeller/VehicleShop/Entities/Cars/CarsIc/SportCar.cs
@@ -1,8 +1,14 @@
 ﻿namespace VehicleShop.Entities.Cars.CarsIc
 {
+    using System;
     using Interfaces;
-    public abstract class SportCar : CarIc , ITuningable
+    public abstract class SportCar : CarIc , ITuningable, IBodyKit
     {
+        public void ApplyBodyKit()
+        {
+            throw new NotImplementedException();
+        }
+
         public void ApplyTuning()
         {
             throw new System.NotImplementedException();
