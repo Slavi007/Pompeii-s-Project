27d6110f2af12366e00376dfbc78424f0f73914a
Pilgrimage <nanobot149@gmail.com>
2017-08-15 21:00:03 +0300

    Have some problem wuth SUV.cs

--- BestVehicleSeller/VehicleShop/Entities/Cars/CarsIc/FamilyCars/Sedan.cs
+++ BestVehicleSeller/VehicleShop/Entities/Cars/CarsIc/FamilyCars/Sedan.cs
@@ -5,7 +5,8 @@
 
     public class Sedan: FamilyCar
     {
-        public Sedan(string carMaker, Brand brand, double maxSpeed, double acceleration, DateTime originDate, decimal price, string colour, double enginePower, int engineVolume, int valveNumber, FuelType fuel, double fuelConsumption) : base(carMaker, brand, maxSpeed, acceleration, originDate, price, colour, enginePower, engineVolume, valveNumber, fuel, fuelConsumption)
+        public Sedan(string carMaker, Brand brand, double maxSpeed, double acceleration, DateTime originDate, decimal price, string colour, double enginePower, int engineVolume, int valveNumber, FuelType fuel, double fuelConsumption) 
+            : base(carMaker, brand, maxSpeed, acceleration, originDate, price, colour, enginePower, engineVolume, valveNumber, fuel, fuelConsumption)
         {
         }
     }
--- BestVehicleSeller/VehicleShop/Entities/Cars/CarsIc/FamilyCars/Wagon.cs
+++ BestVehicleSeller/VehicleShop/Entities/Cars/CarsIc/FamilyCars/Wagon.cs
@@ -5,7 +5,8 @@
 
     public class Wagon : FamilyCar
     {
-        public Wagon(string carMaker, Brand brand, double maxSpeed, double acceleration, DateTime originDate, decimal price, string colour, double enginePower, int engineVolume, int valveNumber, FuelType fuel, double fuelConsumption) : base(carMaker, brand, maxSpeed, acceleration, originDate, price, colour, enginePower, engineVolume, valveNumber, fuel, fuelConsumption)
+        public Wagon(string carMaker, Brand brand, double maxSpeed, double acceleration, DateTime originDate, decimal price, string colour, double enginePower, int engineVolume, int valveNumber, FuelType fuel, double fuelConsumption) 
+            : base(carMaker, brand, maxSpeed, acceleration, originDate, price, colour, enginePower, engineVolume, valveNumber, fuel, fuelConsumption)
         {
         }
     }
