3656e02e95f28986cc7ef9fa7eeaad3bd39c3252
Pilgrimage <nanobot149@gmail.com>
2017-08-15 21:01:03 +0300

    Now problem is solved.

---
+++ BestVehicleSeller/VehicleShop/Entities/Cars/CarsIc/FamilyCars/Suv.cs
@@ -0,0 +1,13 @@
+﻿namespace VehicleShop.Entities.Cars.CarsIc.FamilyCars
+{
+    using System;
+    using VehicleShop.Enums;
+
+    public class Suv : FamilyCar
+    {
+        public Suv(string carMaker, Brand brand, double maxSpeed, double acceleration, DateTime originDate, decimal price, string colour, double enginePower, int engineVolume, int valveNumber, FuelType fuel, double fuelConsumption) 
+            : base(carMaker, brand, maxSpeed, acceleration, originDate, price, colour, enginePower, engineVolume, valveNumber, fuel, fuelConsumption)
+        {
+        }
+    }
+}
