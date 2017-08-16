873f6c8333fc99ba9edd1dfc421389cc3882e667
Pilgrimage <nanobot149@gmail.com>
2017-08-16 17:54:27 +0300

    Class Engine.cs is revised to make it consistent with the number and type of constructor parameters in the base project.

--- BestVehicleSeller/GUI/Engine.cs
+++ BestVehicleSeller/GUI/Engine.cs
@@ -19,68 +19,78 @@ namespace GUI
     {
         public Engine()
         {
-            this.Cars = new List<Car>();
+            this.Cars = new List<CarIc>();
+            this.CarsElectric = new List<CarElectric>();
             this.Motors = new List<Motorcycle>();
         }
 
-        private List<Car> Cars;
+        private List<CarIc> Cars;
+        private List<CarElectric> CarsElectric;
         private List<Motorcycle> Motors;
 
-
-        public void AddCar(string type, string carMaker, string brand, double maxSpeed, double acceleration, DateTime originDate, decimal price, string color)
+        public void AddElectricCar(string type, string carMaker, string brand, double maxSpeed, double acceleration,
+            DateTime originDate, decimal price, string colour, int doors, int seats, int bootCapacity,
+            int batteryCapacity, double motorPower, int range)
 
         {
             if (type == "CityCarElectric")
             {
-                this.Cars.Add(new CityCarElectric(carMaker, brand, maxSpeed, acceleration, originDate, price, color));
+                this.CarsElectric.Add(new CityCarElectric(carMaker, brand, maxSpeed, acceleration, originDate, price, colour,
+                    doors, seats, bootCapacity, batteryCapacity, motorPower, range));
             }
             else if (type == "CruiseCarElectric")
             {
-                this.Cars.Add(new CruiseCarElectric(carMaker, brand, maxSpeed, acceleration, originDate, price, color));
-            }
-            else if (type == "Luxury")
-            {
-                this.Cars.Add(new LuxuryCar(carMaker, brand, maxSpeed, acceleration, originDate, price, color));
-            }
-            else if (type == "Off-Roader")
-            {
-                this.Cars.Add(new OffRoaders(carMaker, brand, maxSpeed, acceleration, originDate, price, color));
-            }
-            else if (type == "Sedan")
-            {
-                this.Cars.Add(new Sedan(carMaker, brand, maxSpeed, acceleration, originDate, price, color));
-            }
-            else if (type == "SUV")
-            {
-                this.Cars.Add(new Suv(carMaker, brand, maxSpeed, acceleration, originDate, price, color));
-            }
-            else if (type == "Wagon")
-            {
-                this.Cars.Add(new Wagon(carMaker, brand, maxSpeed, acceleration, originDate, price, color));
-            }
-            else if (type == "Coupe")
-            {
-                this.Cars.Add(new Coupe(carMaker, brand, maxSpeed, acceleration, originDate, price, color));
-            }
-            else if (type == "Roadster")
-            {
-                this.Cars.Add(new Roadster(carMaker, brand, maxSpeed, acceleration, originDate, price, color));
+                this.CarsElectric.Add(new CruiseCarElectric(carMaker, brand, maxSpeed, acceleration, originDate, price, colour,
+                    doors, seats, bootCapacity, batteryCapacity, motorPower, range));
             }
         }
 
-        public void AddMotor(string type, string carMaker, string brand, double maxSpeed, double acceleration, DateTime originDate, decimal price, string color)
+        public void AddCar(string type, string carMaker, string brand, double maxSpeed, double acceleration, DateTime originDate, decimal price, string colour, int doors, int seats, int bootCapacity, double enginePower, int engineVolume, int valveNumber, string fuelAsString, double fuelConsumption)
+
+        {
+            if (type == "Luxury")
+            {
+                this.Cars.Add(new LuxuryCar(carMaker, brand, maxSpeed, acceleration, originDate, price, colour, doors, seats, bootCapacity, enginePower, engineVolume, valveNumber, fuelAsString, fuelConsumption));
+            }
+            else if (type == "Off-Roader")
+            {
+                this.Cars.Add(new OffRoaders(carMaker, brand, maxSpeed, acceleration, originDate, price, colour, doors, seats, bootCapacity, enginePower, engineVolume, valveNumber, fuelAsString, fuelConsumption));
+            }
+            else if (type == "Sedan")
+            {
+                this.Cars.Add(new Sedan(carMaker, brand, maxSpeed, acceleration, originDate, price, colour, doors, seats, bootCapacity, enginePower, engineVolume, valveNumber, fuelAsString, fuelConsumption));
+            }
+            else if (type == "SUV")
+            {
+                this.Cars.Add(new Suv(carMaker, brand, maxSpeed, acceleration, originDate, price, colour, doors, seats, bootCapacity, enginePower, engineVolume, valveNumber, fuelAsString, fuelConsumption));
+            }
+            else if (type == "Wagon")
+            {
+                this.Cars.Add(new Wagon(carMaker, brand, maxSpeed, acceleration, originDate, price, colour, doors, seats, bootCapacity, enginePower, engineVolume, valveNumber, fuelAsString, fuelConsumption));
+            }
+            else if (type == "Coupe")
+            {
+                this.Cars.Add(new Coupe(carMaker, brand, maxSpeed, acceleration, originDate, price, colour, doors, seats, bootCapacity, enginePower, engineVolume, valveNumber, fuelAsString, fuelConsumption));
+            }
+            else if (type == "Roadster")
+            {
+                this.Cars.Add(new Roadster(carMaker, brand, maxSpeed, acceleration, originDate, price, colour, doors, seats, bootCapacity, enginePower, engineVolume, valveNumber, fuelAsString, fuelConsumption));
+            }
+        }
+
+        public void AddMotor(string type, string carMaker, string brand, double maxSpeed, double acceleration, DateTime originDate, decimal price, string colour, double enginePower, int engineVolume, int valveNumber, string fuelAsString, double fuelConsuption)
         {
             if (type == "Cruiser")
             {
-                this.Motors.Add(new Cruiser(carMaker, brand, maxSpeed, acceleration, originDate, price, color));
+                this.Motors.Add(new Cruiser(carMaker, brand, maxSpeed, acceleration, originDate, price, colour, enginePower, engineVolume, valveNumber, fuelAsString, fuelConsuption));
             }
             else if (type == "Scooter")
             {
-                this.Motors.Add(new Scooter(carMaker, brand, maxSpeed, acceleration, originDate, price, color));
+                this.Motors.Add(new Scooter(carMaker, brand, maxSpeed, acceleration, originDate, price, colour, enginePower, engineVolume, valveNumber, fuelAsString, fuelConsuption));
             }
             else if (type == "StreetSport")
             {
-                this.Motors.Add(new StreetSport(carMaker, brand, maxSpeed, acceleration, originDate, price, color));
+                this.Motors.Add(new StreetSport(carMaker, brand, maxSpeed, acceleration, originDate, price, colour, enginePower, engineVolume, valveNumber, fuelAsString, fuelConsuption));
             }           
         }
 
