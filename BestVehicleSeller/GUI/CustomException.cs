1942a9686dbb220081f96ea209fe06d118864915
Radostin Levov <radostin.levov@gmail.com>
2017-08-16 23:59:24 +0300

    Added CustomException Class.

---
+++ BestVehicleSeller/GUI/CustomException.cs
@@ -0,0 +1,20 @@
+﻿namespace VehicleShop.Core
+{
+    using System.Windows.Forms;
+
+    public static class CustomException
+    {
+        public static void AddException()
+        {
+            MessageBox.Show("Please fill all mandatory fields starting with * ");
+        }
+        public static void SellException()
+        {
+            MessageBox.Show("Please choose vehicle.");
+        }
+        public static void TuningException()
+        {
+            MessageBox.Show("This vehicle alraedy have tuning.");
+        }
+    }
+}
